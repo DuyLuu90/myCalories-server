4f3422bccaa5acedc8e3a36464e6af2dcfe35044
DuyLuu90 <58050608+DuyLuu90@users.noreply.github.com>
2020-09-27 01:51:27 -0700

    update files

--- migrations/002.do.create_meals.sql
+++ migrations/002.do.create_meals.sql
@@ -1,7 +1,14 @@
 CREATE TABLE IF NOT EXISTS meals(
     id SERIAL PRIMARY KEY,
-    userId INTEGER references caloriecounter_users(id) ON DELETE CASCADE NOT NULL,    
-    alldaycalories TEXT,
+    userId INTEGER references caloriecounter_users(id) ON DELETE CASCADE NOT NULL,
+    dateofmeal DATE NOT NULL,
+    alldaycalories INTEGER,
+    breakfast_food TEXT,
+    breakfast_calories INTEGER,
+    lunch_food TEXT,
+    lunch_calories INTEGER,
+    dinner_food TEXT,
+    dinner_calories INTEGER,
     date_created TIMESTAMPTZ NOT NULL DEFAULT now(),
     date_modified TIMESTAMPTZ
 );
--- migrations/004.do.alter_meals_dateofmeal.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE meals
-  ADD COLUMN dateofmeal TIMESTAMPTZ;
