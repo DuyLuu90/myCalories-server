4d18541fa1434900d8d6a65c80515dfece892a22
DuyLuu90 <58050608+DuyLuu90@users.noreply.github.com>
2020-09-27 03:09:09 -0700

    update files

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,13 +3,9 @@ const { DATABASE_URL } = require('./src/config')
 
 module.exports = {
     "migrationDirectory": "migrations",
-    "password": '',
     "driver": "pg",
     "connectionString": (process.env.NODE_ENV === 'test')
       ? process.env.TEST_DATABASE_URL
-      : DATABASE_URL,
-    //"ssl": true,
+      : 'postgres://yhttdkjnivnaon:6814d4f313c85a3d075a92609f64c9a513dad68611fb76bda1ec7d2dd367d38e@ec2-35-173-94-156.compute-1.amazonaws.com:5432/d6vl35rgavo1o',
+    "ssl": true,
   }
-/*
-'postgres://wrpxnebfyculaj:f5229e3f2f950c2bf786b9e3786b9d062e543a929c102d94de5056d4023093e8@ec2-107-22-33-173.compute-1.amazonaws.com:5432/dcu742murmuh29'
- */
